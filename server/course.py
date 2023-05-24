18af1b3ff5d82bb68367d7fe8c193b8f3b5f019b
kmelon55 <kimkyoungmo2869@gmail.com>
2023-05-24 18:20:19 +0900

    Add column in course model

--- server/course.py
+++ server/course.py
@@ -12,28 +12,49 @@ def get_all_courses():
         course_data = {
             'id': course.id,
             'name': course.name,
+            'location': course.location,
+            'subject': course.subject,
+            'target_grade': course.target_grade,
+            'major_seats': course.major_seats,
+            'other_seats': course.other_seats,
+            'minor_seats': course.minor_seats,
+            'professor': course.professor,
+            'schedule': course.schedule,
             'user_id': course.user_id
-            # Add more course attributes as needed
         }
         course_list.append(course_data)
 
     return jsonify({'courses': course_list}), 200
 
 
+
+
 @course_bp.route('/', methods=['POST'])
 def create_course():
     data = request.get_json()
     name = data.get('name')
+    location = data.get('location')
+    subject = data.get('subject')
+    subject_code = data.get('subject_code')
+    target_grade = data.get('target_grade')
+    major_seats = data.get('major_seats')
+    other_seats = data.get('other_seats')
+    minor_seats = data.get('minor_seats')
+    professor = data.get('professor')
+    schedule = data.get('schedule')
     user_id = data.get('user_id')
 
     if not name or not user_id:
         return jsonify({'message': 'Name and user_id are required'}), 400
 
-    course = Course(name=name, user_id=user_id)
+    course = Course(name=name, location=location, subject=subject, subject_code=subject_code,
+                    target_grade=target_grade, major_seats=major_seats, other_seats=other_seats,
+                    minor_seats=minor_seats, professor=professor, schedule=schedule, user_id=user_id)
     db.session.add(course)
     db.session.commit()
 
     return jsonify({'message': 'Course created successfully'}), 201
+
 
 
 @course_bp.route('/<int:course_id>', methods=['GET'])
@@ -46,10 +67,21 @@ def get_course(course_id):
     course_data = {
         'id': course.id,
         'name': course.name,
-        # Add more course attributes as needed
+        'location': course.location,
+        'subject': course.subject,
+        'subject_code': course.subject_code,
+        'target_grade': course.target_grade,
+        'major_seats': course.major_seats,
+        'other_seats': course.other_seats,
+        'minor_seats': course.minor_seats,
+        'professor': course.professor,
+        'schedule': course.schedule,
+        'user_id': course.user_id
     }
 
     return jsonify({'course': course_data}), 200
+
+
 
 @course_bp.route('/<int:course_id>', methods=['PUT'])
 def update_course(course_id):
@@ -63,7 +95,26 @@ def update_course(course_id):
     # Update course data
     if 'name' in data:
         course.name = data['name']
-    # Add more course attributes to update as needed
+    if 'location' in data:
+        course.location = data['location']
+    if 'subject' in data:
+        course.subject = data['subject']
+    if 'subject_code' in data:
+        course.subject_code = data['subject_code']
+    if 'target_grade' in data:
+        course.target_grade = data['target_grade']
+    if 'major_seats' in data:
+        course.major_seats = data['major_seats']
+    if 'other_seats' in data:
+        course.other_seats = data['other_seats']
+    if 'minor_seats' in data:
+        course.minor_seats = data['minor_seats']
+    if 'professor' in data:
+        course.professor = data['professor']
+    if 'schedule' in data:
+        course.schedule = data['schedule']
+    if 'user_id' in data:
+        course.user_id = data['user_id']
 
     db.session.commit()
 
--- server/models.py
+++ server/models.py
@@ -14,13 +14,32 @@ class User(db.Model):
         self.username = username
         self.password = password
 
+
 class Course(db.Model):
     __tablename__ = 'courses'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
+    location = db.Column(db.String(100), nullable=False)  # 수업 장소
+    subject = db.Column(db.String(50), nullable=False)  # 교과 구분
+    subject_code = db.Column(db.String(20), nullable=False)  # 과목 코드
+    target_grade = db.Column(db.String(10), nullable=False)  # 대상 학년
+    major_seats = db.Column(db.Integer, nullable=False)  # 자과 여석 (previously known as science_seats)
+    other_seats = db.Column(db.Integer, nullable=False)  # 타과 여석
+    minor_seats = db.Column(db.Integer, nullable=False)  # 부전공 여석
+    professor = db.Column(db.String(80), nullable=False)  # 담당 교수
+    schedule = db.Column(db.String(100), nullable=False)  # 강의 시간
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    def __init__(self, name, user_id):
+    def __init__(self, name, location, subject, subject_code, target_grade, major_seats, other_seats, minor_seats, professor, schedule, user_id):
         self.name = name
+        self.location = location
+        self.subject = subject
+        self.subject_code = subject_code
+        self.target_grade = target_grade
+        self.major_seats = major_seats
+        self.other_seats = other_seats
+        self.minor_seats = minor_seats
+        self.professor = professor
+        self.schedule = schedule
         self.user_id = user_id
