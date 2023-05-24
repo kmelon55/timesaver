e466ae0dd105e179c5138a1a06db7cb4ee9a9a04
kmelon55 <kimkyoungmo2869@gmail.com>
2023-05-24 15:19:11 +0900

    Update node_modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Alarm from './pages/Alarm';
-import Reco from './pages/Reco';
-import Table from './pages/Table';
-import Login from './pages/Login';
-import Mypage from './pages/Mypage';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Alarm from "./pages/Alarm";
+import Reco from "./pages/Reco";
+import Table from "./pages/Table";
+import Login from "./pages/Login";
+import Mypage from "./pages/Mypage";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/Alarm' element={<Alarm />}/>
-        <Route path='/Reco' element={<Reco />}/>
-        <Route path='/Table' element={<Table />}/>
-        <Route path='/Login' element={<Login />}/>
-        <Route path='/Mypage' element={<Mypage />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/Alarm" element={<Alarm />} />
+        <Route path="/Reco" element={<Reco />} />
+        <Route path="/Table" element={<Table />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Mypage" element={<Mypage />} />
       </Routes>
     </BrowserRouter>
   );
