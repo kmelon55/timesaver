a4dd7f9880454cbd394055a27dfb1cfd8aa197e2
kmelon55 <kimkyoungmo2869@gmail.com>
2023-05-29 16:41:18 +0900

    openai api key

--- server/config.py
+++ server/config.py
@@ -11,4 +11,4 @@ DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db
 SECRET_KEY = "session_test_secret_key"
 
 
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = ""
